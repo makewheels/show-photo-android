b5a987bf26cc00aa23d2d85d0f2b1bf341d53fd4
makewheels <33256842+makewheels@users.noreply.github.com>
2022-09-16 01:33:03 +0800

    准备读相机文件

--- app/src/main/java/com/github/makewheels/android/showphoto/MainActivity.java
+++ app/src/main/java/com/github/makewheels/android/showphoto/MainActivity.java
@@ -1,12 +1,18 @@
 package com.github.makewheels.android.showphoto;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
 
     private File getPhotoFile() {
         return new File(getFilesDir(), "photoFile");
@@ -17,13 +23,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
+
         start();
     }
 
     private void start() {
-        File photoFile = getPhotoFile();
-        if (!photoFile.exists()){
+        //先检查有没有图片，如果没有，从相册选，保存到data
+        if (!getPhotoFile().exists()) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                Uri uri = result.getData().getData();
+                File photoFile = new File(uri.toString());
+                Log.e("tag", photoFile.length() + "");
 
+            }).launch(intent);
         }
     }
 }
